37e696fd265fd13bab4618d415ed7c542808d686
Muhammad Ghous <ghous@umich.edu>
2022-11-17 00:49:50 -0500

    Rearrange/rename columns for better understanding

--- airflow_pipelines/census_data_pipeline/dags/housing_value_airflow_pipeline.py
+++ airflow_pipelines/census_data_pipeline/dags/housing_value_airflow_pipeline.py
@@ -31,7 +31,7 @@ def cleanCensusData():
     dict1 = {'id': 'fips_code'}
     df1.rename(columns=dict1,
           inplace=True)
-    df1.rename(columns={'usd_':'total_owner_occupied_houses', 'usd_Less_than_10000':'usd_less_than_10000'},
+    df1.rename(columns={'usd_':'owner_occupied_houses', 'usd_Less_than_10000':'usd_less_than_10000'},
           inplace=True)
     df1['fips_code'] = df1['fips_code'].str[-5:]
 
@@ -57,8 +57,43 @@ def cleanCensusData():
     #Joining
     joined_df = df3.merge(df1, how='left').merge(df2, how='left')
 
+    rearranged_df = joined_df[['fips_code',
+ 'county',
+ 'state',
+ 'median_2020_housing_value_usd',
+ 'owner_occupied_houses',
+ 'total_occupied',
+ 'total_vacant',
+ 'total_houses',
+ 'usd_less_than_10000',
+ 'usd_10000_to_14999',
+ 'usd_15000_to_19999',
+ 'usd_20000_to_24999',
+ 'usd_25000_to_29999',
+ 'usd_30000_to_34999',
+ 'usd_35000_to_39999',
+ 'usd_40000_to_49999',
+ 'usd_50000_to_59999',
+ 'usd_60000_to_69999',
+ 'usd_70000_to_79999',
+ 'usd_80000_to_89999',
+ 'usd_90000_to_99999',
+ 'usd_100000_to_124999',
+ 'usd_125000_to_149999',
+ 'usd_150000_to_174999',
+ 'usd_175000_to_199999',
+ 'usd_200000_to_249999',
+ 'usd_250000_to_299999',
+ 'usd_300000_to_399999',
+ 'usd_400000_to_499999',
+ 'usd_500000_to_749999',
+ 'usd_750000_to_999999',
+ 'usd_1000000_to_1499999',
+ 'usd_1500000_to_1999999',
+ 'usd_2000000_or_more']]
+
     #enter correct absolute/relavive path before running
-    joined_df.to_csv('/c/Users/Ghous/airflow_linux/output/us_housing_values_cleaned.csv', encoding='utf-8', index=False)
+    rearranged_df.to_csv('/c/Users/Ghous/airflow_linux/output/us_housing_values_cleaned.csv', encoding='utf-8', index=False)
 
 
 with DAG('Clean_housing_Census_Data',
